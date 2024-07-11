b7fa358c91e097a3405262fb554fff7f6492619c
Oliwier Juncewicz <oli.junc@gmail.com>
2024-07-11 18:50:12 +0200

    Moved Type.kt to Presentation package

--- app/src/main/java/com/junclabs/flightsearch/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/junclabs/flightsearch/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.junclabs.flightsearch.ui.theme
+package com.junclabs.flightsearch.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
